6db4958e49d1a2767f2c0a89c82f747308300bcc
Eliel Martins <69322109+martinseliel@users.noreply.github.com>
2023-07-11 12:49:40 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,65 @@
+const textArea = document.querySelector('.text-area');
+const mensagem = document.querySelector('.mensagem');
+
+// As "chaves" de criptografia que utilizaremos são:
+// `A letra "t" é convertida para "spaco"`
+// `A letra "j" é convertida para "jam"`
+// `A letra "r" é convertida para "root"`
+// `A letra "a" é convertida para "aham"`
+// `A letra "u" é convertida para "ufat"`
+
+function btnEncriptar() {
+  const textoEncriptado = encriptar(textArea.value);
+  mensagem.value = textoEncriptado;
+  textArea.value = '';
+}
+
+function encriptar(stringEncriptada) {
+  let matrizCodigo = [
+    ['t', 'spaco'],
+    ['j', 'jam'],
+    ['r', 'root'],
+    ['a', 'aham'],
+    ['u', 'ufat'],
+  ];
+  stringEncriptada = stringEncriptada.toLowerCase();
+
+  for (let i = 0; i < matrizCodigo.length; i++) {
+    if (stringEncriptada.includes(matrizCodigo[i][0])) {
+      stringEncriptada = stringEncriptada.replaceAll(
+        matrizCodigo[i][0],
+        matrizCodigo[i][1]
+      );
+    }
+  }
+
+  return stringEncriptada;
+}
+
+function btnDesencriptar() {
+  const textoDesencriptado = desencriptar(textArea.value);
+  mensagem.value = textoDesencriptado;
+  textArea.value = '';
+}
+
+function desencriptar(stringDesencriptada) {
+  let matrizCodigo = [
+    ['e', 'enter'],
+    ['i', 'imes'],
+    ['a', 'ai'],
+    ['o', 'ober'],
+    ['u', 'ufat'],
+  ];
+  stringDesencriptada = stringDesencriptada.toLowerCase();
+
+  for (let i = 0; i < matrizCodigo.length; i++) {
+    if (stringDesencriptada.includes(matrizCodigo[i][1])) {
+      stringDesencriptada = stringDesencriptada.replaceAll(
+        matrizCodigo[i][1],
+        matrizCodigo[i][0]
+      );
+    }
+  }
+
+  return stringDesencriptada;
+}
